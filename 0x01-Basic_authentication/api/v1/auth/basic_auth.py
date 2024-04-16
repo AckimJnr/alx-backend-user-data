3b11229f74d6173f98d790ba6778757faf431ab0
AckimJnr <acklongwe@gmail.com>
2024-04-17 00:16:10 +0200

    Add base 64 authorisation

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,4 +8,14 @@ class BasicAuth(Auth):
     """
     Implements basic authentication
     """
-    pass
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """
+        extract base64 authorization header
+        """
+        if authorization_header is None or not isinstance(
+                                authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header[6:]
